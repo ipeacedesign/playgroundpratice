41785d952fa0d3ce68e81eff1ee6957c1423f3b3
Kelly Wang <kellywang@Kellyde-MacBook-Air.local>
2017-03-10 00:23:32 +0800

    Initial Commit

---
+++ if else pratice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  if else pratice
+//
+//  Created by Kelly Wang on 2017/3/10.
+//  Copyright © 2017年 Kelly Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
